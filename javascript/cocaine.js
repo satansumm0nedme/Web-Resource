06b180a02a01f860a674daaa1df7b7ed24ec2bf8
Ray <161750819+satansumm0nedme@users.noreply.github.com>
2024-03-18 04:53:47 -0400

    Add files via upload

---
+++ javascript/cocaine.js
@@ -0,0 +1,377 @@
+window.onscroll = function() {
+  myFunction();
+};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".carrie", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".carrie",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".salem", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  delay: 0.5, 
+  scrollTrigger: {
+    trigger: ".salem",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".shining", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".shining",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".rage", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".rage",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".stand", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".stand",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".walk", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".walk",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".dead", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".dead",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".firestarter", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".firestarter",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".roadwork", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".roadwork",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".cujo", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".cujo",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".running", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".running",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".tower", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".tower",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".christine", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".christine",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".pet", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".pet",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".werewolf", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".werewolf",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".talisman", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".talisman",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".dragon", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".dragon",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".thinner", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".thinner",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".it", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".it",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".tower2", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".tower2",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".misery", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".misery",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".tommyknockers", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".tommyknockers",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".outside", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".outside",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".elevation", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".elevation",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".institute", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".institude",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".later", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".later",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".billy", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".billy",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".gwendy", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".gwendy",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".fairy", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".fairy",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
+
+gsap.from(".holly", {
+  y: 100,
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: {
+    trigger: ".holly",
+    start: "top bottom",
+    end: "bottom center",
+  },
+  stagger: 0.3
+});
